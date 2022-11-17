26a161918eb58dd0bcbe410a4e6cff5979a8ca81
Alessandro Ricci <a.ricci@exolab.it>
2022-11-17 15:08:51 +0100

    Create repo

---
+++ src/test/java/com/example/exoflix/ExoflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exoflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
